8fe9308889673c63e6a27d73fef54bba35ecf5c8
Chungsang Tom Lam <erinata@gmail.com>
2023-01-31 12:05:06 -0500

    add kfold template

---
+++ kfold_template.py
@@ -0,0 +1,31 @@
+from sklearn.model_selection import KFold
+from sklearn import metrics
+
+def run_kfold(data, target, machine, n):
+  print("run kfold")
+  kfold_object = KFold(n_splits=n)
+  kfold_object.get_n_splits(data)
+
+  print(kfold_object)
+
+  i = 0
+  for train_index, test_index in kfold_object.split(data):
+    i=i+1
+    print("Round: ", str(i))
+    print("Training index: ")
+    print(train_index)
+    print("Testing index: ")
+    print(test_index)
+    data_train = data[train_index]
+    target_train = target[train_index]
+    data_test = data[test_index]
+    target_test = target[test_index]
+    machine.fit(data_train, target_train)
+    
+    prediction = machine.predict(data_test)
+    r2 = metrics.r2_score(target_test, prediction)
+    print("R square score: ", r2) 
+    print("\n\n")
+  
+if __name__ == '__main__':
+  run_kfold()
