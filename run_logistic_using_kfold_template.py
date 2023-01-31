6eeffbac97ea5097ac2c8d6ecd8b40f179b589ed
Chungsang Tom Lam <erinata@gmail.com>
2023-01-31 12:13:55 -0500

    run logistic

---
+++ run_logistic_using_kfold_template.py
@@ -0,0 +1,13 @@
+import pandas 
+from sklearn import linear_model
+
+import kfold_template
+
+dataset = pandas.read_csv("dataset.csv")
+
+target = dataset.iloc[:,1].values
+data = dataset.iloc[:,3:9].values
+
+machine = linear_model.LogisticRegression()
+
+kfold_template.run_kfold(data, target, machine, 4)
